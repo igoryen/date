0891b613995ebfc7a30eb1d2202d3c0397ed7d17
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-02-19 15:06:08 -0500

    200219-1505 - factored out the header links

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link> |
+            <router-link to="/user">User</router-link>
+        </div>
+    </div>
+</template>
