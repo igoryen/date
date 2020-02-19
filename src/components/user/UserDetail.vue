a83076ea974c09c49a5659bd3e382dba6d49512d
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-02-19 14:35:25 -0500

    245. Making Router Links more Dynamic

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,5 +2,6 @@
     <div>
         <h3>Some User Details</h3>
         <p>The loaded user's ID: {{ $route.params.id }}</p>
+        <router-link tag="button" v-bind:to="'/user/' + $route.params.id + '/edit'">Edit User</router-link>
     </div>
 </template>
