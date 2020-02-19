558dc25c752bbf4b0c34d60edc7ce0d0bd0cb02c
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-02-19 13:16:04 -0500

    238. Navigating from Code (Imperative Navigation)

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,21 @@
 <template>
-    <h1>The User Page</h1>
-
+    <div>
+        <h1>The User Page</h1>
+        <hr />
+        <button @click="navigateToHome">Go Home</button>
+    </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        navigateToHome() {
+            this.$router.push('/');
+            // $router is provided by the router package.
+            // .push() as in 'push this route on the stack of existing routes
+            // so that the < and > buttons in the browser will work
+            // because the old route will be saved as well
+        }
+    }
+};
+</script>
