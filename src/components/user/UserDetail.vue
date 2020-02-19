074627da9873e8c8dd3d2e8ac9dfbf5d8ca5a9b7
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-02-19 14:53:47 -0500

    247. Using Query Parameters

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,6 +2,16 @@
     <div>
         <h3>Some User Details</h3>
         <p>The loaded user's ID: {{ $route.params.id }}</p>
-        <router-link tag="button" v-bind:to="{ name: 'userEdit', params: { id: $route.params.id } }">Edit User</router-link>
+        <router-link
+            tag="button"
+            v-bind:to="{
+                name: 'userEdit',
+                params: { id: $route.params.id },
+                query: {
+                    locale: 'en',
+                    q: 100
+                }
+            }"
+        >Edit User</router-link>
     </div>
 </template>
