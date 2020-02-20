83385cd21fbee78295a008cfafc184f348b4893a
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-02-19 20:56:27 -0500

    252. Passing the Hash Fragment

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -4,14 +4,24 @@
         <p>The loaded user's ID: {{ $route.params.id }}</p>
         <router-link
             tag="button"
-            v-bind:to="{
-                name: 'userEdit',
-                params: { id: $route.params.id },
-                query: {
-                    locale: 'en',
-                    q: 100
-                }
-            }"
+            v-bind:to="link"
         >Edit User</router-link>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            link: {
+                name: 'userEdit',
+                params: { id: this.$route.params.id },
+                query: {
+                    locale: 'en',
+                    q: 100
+                },
+                hash: '#data'
+            }
+        }
+    }
+}
+</script>
