57d37168ca461891d3cbb5288648825534048d26
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-03 00:57:59 -0400

    124. Passing Content with Slots

--- src/components/quote/Quote.vue
+++ src/components/quote/Quote.vue
@@ -1,9 +1,13 @@
 <template>
     <div>
         <h1>The Quote Page</h1>
-        <app-quote quote="A wonderful quote!!!"></app-quote>
+        <app-quote>
+            <div class="aba">
+                <h2>The Quote</h2>
+                <p>My quote!</p>
+            </div>
+        </app-quote>
         <router-view />
-
         <hr />
         <button @click="navigateToHome">Go Home</button>
     </div>
--- src/components/quote/QuoteDetail.vue
+++ src/components/quote/QuoteDetail.vue
@@ -1,17 +1,15 @@
 <template>
     <div class="aaa">
-        <div class="aba">
-            <p>{{ quote }}</p>
-        </div>
+        <slot></slot>
     </div>
 </template>
 <script>
 export default {
-    props: ['quote']
 };
 </script>
 <style lang="scss" scoped>
 .aba {
+    background-color: aqua;
     border: 1px solid #ccc;
     box-shadow: 1px 1px 1px black;
     margin: 30px auto;
