865feca4391c60307c69902d782c16e5e947c3bd
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-07 19:12:12 -0400

    172. Using Multiple Modifiers

--- src/components/directives/Directives.vue
+++ src/components/directives/Directives.vue
@@ -7,6 +7,7 @@
                 <p v-highlight:background="'#ccc'">Color this</p>
                 <p v-highlight:background.delayed="'pink'">Colored with delay</p>
                 <p v-local-highlight:background.delayed="'orange'">Local custom directive</p>
+                <p v-local-highlight:background.delayed.blink="'#ccffff'">Local. Blink.</p>
             </div>
         </div>
         <div>
@@ -25,16 +26,35 @@ export default {
         "local-highlight": {
             bind(el, binding /*, vnode*/) {
                 var delay = 0;
+
                 if (binding.modifiers["delayed"]) {
                     delay = 4000;
                 }
-                setTimeout(() => {
-                    if (binding.arg == "background") {
-                        el.style.backgroundColor = binding.value;
-                    } else {
-                        el.style.color = binding.value;
-                    }
-                }, delay);
+                if (binding.modifiers["blink"]) {
+                    let mainColor = binding.value;
+                    let secondColor = "#b3ffb3"; // light green
+                    let currentColor = mainColor;
+                    setTimeout(() => {
+                        setInterval(() => {
+                            currentColor == secondColor
+                                ? (currentColor = mainColor)
+                                : (currentColor = secondColor);
+                            if (binding.arg == "background") {
+                                el.style.backgroundColor = currentColor;
+                            } else {
+                                el.style.color = currentColor;
+                            }
+                        }, 1000);
+                    }, delay);
+                } else {
+                    setTimeout(() => {
+                        if (binding.arg == "background") {
+                            el.style.backgroundColor = binding.value;
+                        } else {
+                            el.style.color = binding.value;
+                        }
+                    }, delay);
+                }
             }
         }
     }
