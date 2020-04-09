24266e1e873a66a1368351093d1028647d556147
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-09 17:17:02 -0400

    178. Creating a Local Filter

--- src/components/filmix/Filmix.vue
+++ src/components/filmix/Filmix.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="filmix">
-        <h1>Filters & Mixins</h1>   
+        <h1>Filters & Mixins</h1>
+        <p>{{ text | toUppercase }}</p>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            text: "Hello there!"
+        }
+    },
+    filters: {
+        // 'to-uppercase'() // also possible
+        toUppercase(value) {
+            return value.toUpperCase();
+        }
+    }
 }
 </script>
 <style lang="sass" scoped>
