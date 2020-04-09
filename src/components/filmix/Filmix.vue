a763d81673951ac0da2dc98a6beb7f9fd3d796c9
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-09 17:36:07 -0400

    180. An (often-times better) Alternative to Filters: Computed Properties

--- src/components/filmix/Filmix.vue
+++ src/components/filmix/Filmix.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="filmix">
         <h1>Filters & Mixins</h1>
+        <hr>
+        <input type="text" v-model="filterText">
+        <ul>
+            <li v-for="(fruit, index) in filteredFruits" :key="index"> {{ fruit }}</li>
+        </ul>
+        <hr>
         <p>{{ text | toUppercase | to-lowercase }}</p>
     </div>
 </template>
@@ -8,6 +14,8 @@
 export default {
     data() {
         return {
+            fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+            filterText: '',
             text: "Hello there!"
         }
     },
@@ -16,9 +24,23 @@ export default {
         toUppercase(value) {
             return value.toUpperCase();
         }
+    },
+    computed: {
+        filteredFruits() {
+            return this.fruits.filter((element) => {
+                return element.match(this.filterText);
+            });
+        }
     }
 }
 </script>
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+.filmix {
+    ul {
+        padding: 0;
+        li {
+            list-style: none;
+        }
+    }
+}
 </style>
