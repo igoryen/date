8a6b6e8b7bc620ea33262d16b72332e1413726fd
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-12 13:47:35 -0400

    195. Creating a "Slide" Transition with the CSS Animation Property

--- src/components/transitions/Transitions.vue
+++ src/components/transitions/Transitions.vue
@@ -5,7 +5,10 @@
         <button @click="show = !show">Show an alert()</button>
         <br><br>
         <transition name="fade">
-            <div v-if="show">some alert</div>
+            <div v-if="show">fading alert</div>
+        </transition>
+        <transition name="slide">
+            <div v-if="show">sliding alert</div>
         </transition>
     </div>
 </template>
@@ -33,6 +36,35 @@ export default {
                 transition: opacity 1s;
                 opacity: 0;
             }
+        }
+    }
+    .slide {
+        &-enter {
+            &-active {
+                animation: slide-in 1s ease-out forwards;
+            }
+        }
+        &-leave {
+            &-active {
+                animation: slide-out 1s ease-out forwards;
+            }
+        }
+    }
+
+    @keyframes slide-in {
+        from{
+            transform: translateY( 20px );
+        }
+        to {
+            transform: translateY( 0 );
+        }
+    }
+    @keyframes slide-out {
+        from{
+            transform: translateY( 0 );
+        }
+        to {
+            transform: translateY( 20px );
         }
     }
 </style>
