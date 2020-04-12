941fcec422916bc08b675f86e1ee91163bed3dae
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-12 13:15:17 -0400

    191. Preparing Code to use Transitions (have a simple show-hide)

--- src/components/transitions/Transitions.vue
+++ src/components/transitions/Transitions.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="trans">
         <h1>Transitions</h1>
+        <h1></h1>
+        <button @click="show = !show">Show an alert()</button>
+        <br><br>
+        <div v-if="show">some alert</div>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            show: false
+        }
+    }
 }
 </script>
 
