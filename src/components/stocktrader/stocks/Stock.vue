950107188f0a7905a4d3b0ea8dccecf94782be25
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-16 11:13:38 -0400

    289. Creating Stocks Components

--- src/components/stocktrader/stocks/Stock.vue
+++ src/components/stocktrader/stocks/Stock.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="trader">
-        <div>
-            <h3>stocks/stock</h3>
+    <div class="stock">
+        <h4>NAME <small>(Price: PRICE)</small></h4>
+        <div class="body">
+            <input type="number" placeholder="quantity">
+            <button>Buy</button>
         </div>
     </div>
 </template>
@@ -12,6 +14,24 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    h4 {
+        background-color: rgb(212, 255, 241);
+        margin: 0;
+    }
+    .stock { 
+        border-radius: 3px; 
+        border: 1px solid aquamarine;
+        margin: 0.5em;
+        padding: 0.1em;
+        width: 25%;
+    }
+    .body {
+        display: flex;
+        justify-content: space-between;
+        padding: 3px;
+        input {
+            max-width: 5em;
+        }
+    }
 </style>
--- src/components/stocktrader/stocks/Stocks.vue
+++ src/components/stocktrader/stocks/Stocks.vue
@@ -3,15 +3,34 @@
         <div>
             <h2>Stock Trader - Stocks</h2>
         </div>
+        <div class="stocks-box">
+            <app-stock v-for="(stock, idx) in stocks" :key="idx"></app-stock>
+        </div>
     </div>
 </template>
 
 <script>
+import stock from './Stock.vue';
 export default {
-
+    data() {
+        return {
+            stocks: [
+                { id: 1, name: 'BMW', price: 110 },
+                { id: 2, name: 'IBM', price: 220 },
+                { id: 3, name: 'CNN', price: 330 },
+                { id: 4, name: 'NBS', price: 440 },
+            ]
+        }
+    },
+    components: {
+        appStock: stock
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    .stocks-box {
+        display: flex;
+        flex-wrap: wrap;
+    }
 </style>
