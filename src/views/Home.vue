827aa06a254763723b7af2cd2a36eeeead0b97a1
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-19 18:59:49 -0400

    316. Time to Practice (1) - Outputting Data to Templates (Solution)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,16 @@
     <div class="home">
         <pre>Hello from Home.vue</pre>
         <HelloWorld msg="Welcome to Your Vue.js App" />
+        <div>
+            <p>Name: {{ name }}</p>
+            <p>Age: {{ age }}</p>
+            <p>Age times 3 = {{ age * 3 }}</p>
+            <p>Random number: {{ random() }}</p>
+            <p style="width: 100px;">
+                <img :src="img" alt="">
+            </p>
+            <input type="text" :value="txt">
+        </div>
     </div>
 </template>
 
@@ -13,6 +23,19 @@ export default {
     name: "Home",
     components: {
         HelloWorld
+    },
+    data() {
+        return {
+            name: 'igoryen',
+            age: 11,
+            img: 'http://www.valuecoders.com/blog/wp-content/uploads/2016/11/Vue.js-cover-150x105.png',
+            txt: 'i am an input'
+        }
+    },
+    methods: {
+        random: function() {
+            return Math.random();
+        }
     }
 };
 </script>
