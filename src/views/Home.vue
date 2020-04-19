add51d8af6c13eb7b3a6460df952d78607ffb4f2
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-19 19:17:57 -0400

    319. Time to Practice (2) - Events (Solution)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,20 @@
     <div class="home">
         <pre>Hello from Home.vue</pre>
         <HelloWorld msg="Welcome to Your Vue.js App" />
-        <div>
-            <p>Name: {{ name }}</p>
-            <p>Age: {{ age }}</p>
-            <p>Age times 3 = {{ age * 3 }}</p>
-            <p>Random number: {{ random() }}</p>
-            <p style="width: 100px;">
-                <img :src="img" alt="">
-            </p>
+        <div class="spans">
+            <span>Name: {{ name }}</span>
+            <span>Age: {{ age }}</span>
+            <span>Age times 3 = {{ age * 3 }}</span>
+            <span>Random number: {{ random() }}</span>
+            <span>
+                <img :src="img" alt=""  style="width: 30px;">
+            </span>
             <input type="text" :value="txt">
+            <button @click="alertme">Show alert</button>
+            <input @keydown="value = $event.target.value" type="text" placeholder="as you type">
+            <span class="output">{{ value }}</span>
+            <input @keydown.enter="val2 = $event.target.value" type="text" placeholder="on click Enter">
+            <span class="output">{{ val2 }}</span>
         </div>
     </div>
 </template>
@@ -29,13 +34,34 @@ export default {
             name: 'igoryen',
             age: 11,
             img: 'http://www.valuecoders.com/blog/wp-content/uploads/2016/11/Vue.js-cover-150x105.png',
-            txt: 'i am an input'
+            txt: 'i am an input',
+            value: '',
+            val2: ''
         }
     },
     methods: {
         random: function() {
             return Math.random();
+        },
+        alertme: function() {
+            alert('Hi!')
         }
     }
 };
 </script>
+<style lang="scss" scoped>
+    .spans {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-content: flex-start;
+        span, input, button {
+            border: 1px solid #ccc;
+            margin: 3px;
+            padding: 3px;
+        }
+    }
+    .output {
+        background-color: antiquewhite;
+    }
+</style>
