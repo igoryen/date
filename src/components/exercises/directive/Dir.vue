c0a5a5659976e3c486e0972fdfdd4896981f325d
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-21 16:29:04 -0400

    338-b. Time to Practice (10) - Directives (Solution)

--- src/components/exercises/directive/Dir.vue
+++ src/components/exercises/directive/Dir.vue
@@ -1,12 +1,26 @@
 <template>
     <div class="ex-dir">
         <strong>Directive</strong>
+        <button v-customOn="clicked">Click me</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    directives: {
+        customOn: {
+            bind( el, binding ) {
+                el.onclick = () => {
+                    binding.value();
+                }
+            }
+        }
+    },
+    methods: {
+        clicked() {
+            alert('I was clicked')
+        }
+    }
 }
 
 </script>
