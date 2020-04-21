6f7815305bcba4471944b16dab5e54629d3c00ce
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-21 16:54:55 -0400

    340-b. Time to Practice (11) - Filters and Mixins (Solution)

--- src/components/exercises/filmix/Fm.vue
+++ src/components/exercises/filmix/Fm.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="ex-fm">
         <strong>Filters and Mixins</strong>
-        <p>some text</p>
+        <p>redrum desserts live stink sports larger pupils raw smart pals guns lived dog straw time deliver star spoons</p>
+        <p>{{ 'redrum desserts live stink sports larger pupils raw smart pals guns lived dog straw time deliver star spoons' | reverse }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    
+    filters: {
+        reverse(val) {
+            return val.split("").reverse().join("");
+        }
+    }
 }
 
 </script>
