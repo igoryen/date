feec522370bdba65e9d4ad02ef9bb8dc01afb923
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-22 17:45:01 -0400

    Tidied up Styles.vue

--- src/components/exercises/Styles.vue
+++ src/components/exercises/Styles.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="styles">
-        
-        <hr>
+        <strong>Styles.vue</strong>
+        <hr />
         <span :class="[float, 'blue', 'text-color']">I've got no class</span>
-        <input v-model="userClass" type="text">
-        <span :class="[{visible: true}, userClass]"></span>
-        <input v-model="userClass" type="text">
-        <input v-model="isVisible" type="text">
-        <input :class="[{visible: isVisible}, userClass]" type="text">
-        <input v-model="myStyle.backgroundColor" type="text" placeholder="Type color name">
-        <span :style="myStyle"></span>
         <hr>
-        
+        <input v-model="userClass" type="text" />
+        <span :class="[{visible: true}, userClass]"></span>
+        <hr>
+        <input v-model="isVisible" type="text" />
+        <input :class="[{visible: isVisible}, userClass]" type="text" />
+        <hr>
+        <p>Type a browser-supported color name:</p>
+        <input v-model="myStyle.backgroundColor" type="text" placeholder="Type color name" />
+        <span :style="myStyle"></span>
+        <hr />
     </div>
 </template>
 
@@ -19,22 +21,19 @@
 export default {
     data: function() {
         return {
-            
-            float: 'float',
-            userClass: '', // userClass = user class
+            float: "float",
+            userClass: "", // userClass = user class
             isVisible: true,
             myStyle: {
-                border: '1px solid black',
-                width: '100px',
-                height: '20px',
-                backgroundColor: 'gray'
-            },            
-        }
-    },
-
-}
+                border: "1px solid black",
+                width: "100px",
+                height: "20px",
+                backgroundColor: "gray"
+            }
+        };
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-    
 </style>
