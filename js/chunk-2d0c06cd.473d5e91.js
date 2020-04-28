45d027d1e85d6baef3a527c64949ccd2f8d7ffc5
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-28 15:11:22 -0400

    added public path (as per https://cli.vuejs.org/guide/deployment.html#github-pages)

---
+++ js/chunk-2d0c06cd.473d5e91.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c06cd"],{4270:function(e,n,o){"use strict";o.r(n);var t=function(){var e=this,n=e.$createElement,o=e._self._c||n;return o("div",[o("h1",[e._v("The User Page")]),o("router-view"),o("hr"),o("button",{on:{click:e.navigateToHome}},[e._v("Go Home")])],1)},u=[],c={methods:{navigateToHome:function(){this.$router.push({name:"Home"})}}},r=c,a=o("2877"),i=Object(a["a"])(r,t,u,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0c06cd.473d5e91.js.map
